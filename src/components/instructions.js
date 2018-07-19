34312391d60a837f1f84a31e44ae78f6dd3e023a
ekno27 <edgar.canozelaya.6@my.csun.edu>
2018-07-18 17:59:23 -0700

    changed message

--- src/components/instructions.js
+++ src/components/instructions.js
@@ -10,7 +10,7 @@ const Instructions = (props)=> {
       <div className="card blue-grey darken-1">
         <div className="card-content white-text">
           <span className="card-title center"><h3><strong>Score: {props.score} | Top Score: {props.maxScore}</strong></h3></span>
-          <h5 className="center">Click on an image, if you click on that image again you lose your points! </h5>
+          <h5 className="center">Click on an album cover, if you click on that album cover again you lose your points! </h5>
         </div>
       </div>
     
